607a60ba321db2fadd7d88e7db277112a87bd1e1
Tiaunjia <abc.tiangia@gmail.com>
2016-10-06 14:56:06 -0400

    Minor formatting corrections

--- D1L1/srm258_AutoLoan.cpp
+++ D1L1/srm258_AutoLoan.cpp
@@ -13,78 +13,78 @@ class AutoLoan
 {
 public:
 
-	// Predicate for binary search: Is the yearly rate >= target yearly Rate?
-	bool p(double yearlyRate, double price, double monthlyPayment, int loanTerm, double epsilon)
-	{
-		double balance = price;
-		double monthlyRate = yearlyRate/12.0/100.0;
+  // Predicate for binary search: Is the yearly rate >= target yearly Rate?
+  bool p(double yearlyRate, double price, double monthlyPayment, int loanTerm, double epsilon)
+  {
+    double balance = price;
+    double monthlyRate = yearlyRate/12.0/100.0;
 
-		// printf("\033[94m balance: %.2f \n", balance);
-		// printf("\033[94m monthlyRate: %.2f \n", monthlyRate);
-		// printf("\033[94m yearlyRate: %.2f \n", yearlyRate);
+    // printf("\033[94m balance: %.2f \n", balance);
+    // printf("\033[94m monthlyRate: %.2f \n", monthlyRate);
+    // printf("\033[94m yearlyRate: %.2f \n", yearlyRate);
 
-		for(int i=0; i<loanTerm; i++)
-		{
-			// printf("\033[91m interest: %.2f \n", balance * (1.0 + monthlyRate));
-			balance = (balance * (1.0 + monthlyRate)) - monthlyPayment;
-		}
+    for(int i=0; i<loanTerm; i++)
+    {
+      // printf("\033[91m interest: %.2f \n", balance * (1.0 + monthlyRate));
+      balance = (balance * (1.0 + monthlyRate)) - monthlyPayment;
+    }
 
-		// printf("balance: %.8f \n\n", balance);
-		return balance > epsilon; // This is tricky - ensure you revise on paper how to move bounds!
-	}
+    // printf("balance: %.8f \n\n", balance);
+    return balance > epsilon; // This is tricky - ensure you revise on paper how to move bounds!
+  }
 
-	double interestRate(double price, double monthlyPayment, int loanTerm)
-	{
-		double lo = 0.0;
-		double hi = 100.0;
-		int iterations = 100;
-		double epsilon = 1e-9;
+  double interestRate(double price, double monthlyPayment, int loanTerm)
+  {
+    double lo = 0.0;
+    double hi = 100.0;
+    int iterations = 100;
+    double epsilon = 1e-9;
 
-		while (iterations > 0)
-		{
-			double mid = (lo + hi)/2; // current yearly interest rate
-			// printf("mid: %.2f, lo: %.2f, hi: %.2f \n", mid, lo, hi);
-			if (p(mid, price, monthlyPayment, loanTerm, epsilon))
-				hi = mid;
-			else
-				lo = mid;
+    while (iterations > 0)
+    {
+      double mid = (lo + hi)/2; // current yearly interest rate
+      // printf("mid: %.2f, lo: %.2f, hi: %.2f \n", mid, lo, hi);
+      if (p(mid, price, monthlyPayment, loanTerm, epsilon))
+        hi = mid;
+      else
+        lo = mid;
 
-			iterations--;
-		}
+      iterations--;
+    }
 
-		return lo;
-	}
+    return lo;
+  }
 };
 
 int main()
 {
-	AutoLoan a;
-	double price, monthlyPayment, yearlyRate;
-	int loanTerm;
+  AutoLoan a;
+  double price, monthlyPayment, yearlyRate;
+  int loanTerm;
 
-	// SRM Problem Unit Test 1
-	price = 6800;
-	monthlyPayment = 100;
-	loanTerm = 68;
+  // SRM Problem Unit Test 1
+  price = 6800;
+  monthlyPayment = 100;
+  loanTerm = 68;
 
-	yearlyRate = a.interestRate(price, monthlyPayment, loanTerm);
-	printf("\033[92m Yearly Rate: \033[m %.8f \n", yearlyRate);
+  yearlyRate = a.interestRate(price, monthlyPayment, loanTerm);
+  printf("\033[92m Yearly Rate: \033[m %.8f \n", yearlyRate);
 
-	// SRM Problem Unit Test 2
-	price = 2000;
-	monthlyPayment = 510;
-	loanTerm = 4;
+  // SRM Problem Unit Test 2
+  price = 2000;
+  monthlyPayment = 510;
+  loanTerm = 4;
 
-	yearlyRate = a.interestRate(price, monthlyPayment, loanTerm);
-	printf("\033[92m Yearly Rate: \033[m %.8f \n", yearlyRate);
+  yearlyRate = a.interestRate(price, monthlyPayment, loanTerm);
+  printf("\033[92m Yearly Rate: \033[m %.8f \n", yearlyRate);
 
-	// SRM Problem Unit Test 3
-	price = 15000;
-	monthlyPayment = 364;
-	loanTerm = 48;
+  // SRM Problem Unit Test 3
+  price = 15000;
+  monthlyPayment = 364;
+  loanTerm = 48;
 
-	yearlyRate = a.interestRate(price, monthlyPayment, loanTerm);
-	printf("\033[92m Yearly Rate: \033[m %.8f \n", yearlyRate);
+  yearlyRate = a.interestRate(price, monthlyPayment, loanTerm);
+  printf("\033[92m Yearly Rate: \033[m %.8f \n", yearlyRate);
 
 }
 
