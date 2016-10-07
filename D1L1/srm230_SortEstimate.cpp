92c7ed3d7e08996d57961b800e048ad1c1a6312e
Tiaunjia <abc.tiangia@gmail.com>
2016-10-07 08:29:00 -0400

    Minor formatting corrections

--- D1L1/srm230_SortEstimate.cpp
+++ D1L1/srm230_SortEstimate.cpp
@@ -15,64 +15,64 @@
 class SortEstimate
 {
 public:
-	bool p(int c, int t, double n)
-	{
-		return (c * n * log2(n)) >= t;
-	}
+  bool p(int c, int t, double n)
+  {
+    return (c * n * log2(n)) >= t;
+  }
 
-	double howMany(int c, int time)
-	{
-		double lo = 0;
-		double hi = 1000000000; //std::numeric_limits<double>::max(); - max double causes overflow
-		int iterations = 100;
+  double howMany(int c, int time)
+  {
+    double lo = 0;
+    double hi = 1000000000; //std::numeric_limits<double>::max(); - max double causes overflow
+    int iterations = 100;
 
-		for(int i=0; i<iterations; i++)
-		{
-			double mid = (lo + hi)/2;
-			if (p(c, time, mid))
-				hi = mid;
-			else
-				lo = mid;
-		}
+    for(int i=0; i<iterations; i++)
+    {
+      double mid = (lo + hi)/2;
+      if (p(c, time, mid))
+        hi = mid;
+      else
+        lo = mid;
+    }
 
-		return lo;
-	}
+    return lo;
+  }
 
 };
 
 
 int main()
 {
-	SortEstimate s;
-	int c, t;
-	double n;
+  SortEstimate s;
+  int c, t;
+  double n;
 
-	// SRM Unit Test 1
-	c = 1;
-	t = 8;
+  // SRM Unit Test 1
+  c = 1;
+  t = 8;
 
-	n = s.howMany(c, t);
-	printf("\033[92m n: \033[m %.8f \n", n);
+  n = s.howMany(c, t);
+  printf("\033[92m n: \033[m %.8f \n", n);
 
-	// SRM Unit Test 2
-	c = 2;
-	t = 16;
+  // SRM Unit Test 2
+  c = 2;
+  t = 16;
 
-	n = s.howMany(c, t);
-	printf("\033[92m n: \033[m %.8f \n", n);
+  n = s.howMany(c, t);
+  printf("\033[92m n: \033[m %.8f \n", n);
 
 
-	// SRM Unit Test 3
-	c = 37;
-	t = 12392342;
+  // SRM Unit Test 3
+  c = 37;
+  t = 12392342;
 
-	n = s.howMany(c, t);
-	printf("\033[92m n: \033[m %.8f \n", n);
+  n = s.howMany(c, t);
+  printf("\033[92m n: \033[m %.8f \n", n);
 
-	// SRM Unit Test 4
-	c = 1;
-	t = 2000000000;
+  // SRM Unit Test 4
+  c = 1;
+  t = 2000000000;
 
-	n = s.howMany(c, t);
-	printf("\033[92m n: \033[m %.8f \n", n);
+  n = s.howMany(c, t);
+  printf("\033[92m n: \033[m %.8f \n", n);
 }
